6d134b8ee068b382f7e96d0b3913fc521ce26bf3
André Abadesso <andre.abadesso@gmail.com>
2024-01-22 12:40:07 -0300

    docs: added docstrings to the methods in UnleashClietn

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,18 @@ export default class UnleashClient extends EventEmitter {
     this.toggles = null;
   }
 
-  private getHeaders() {
+  /**
+   * Generates headers for HTTP requests.
+   * 
+   * Constructs and returns a set of HTTP headers, including authorization and content type.
+   * If an ETag is available, it is also included to manage caching.
+   *
+   * This method is intended for internal use within the class to prepare headers
+   * for HTTP requests.
+   *
+   * @returns  An object representing HTTP headers.
+   */
+  private getHeaders(): Record<string, string> {
     const headers: IHeaders = {
       Authorization: this.clientKey,
       Accept: 'application/json',
@@ -47,6 +58,21 @@ export default class UnleashClient extends EventEmitter {
     return headers as unknown as Record<string, string>;
   }
 
+  /**
+   * Asynchronously fetches toggle data from the feature toggle provider
+   * 
+   * If the request is successful and the response status is not 304 (Not Modified),
+   * it updates the instance's `etag` and `toggles` properties with the new data from the response.
+   * 
+   * The method returns a `FetchTogglesStatus` indicating whether the toggles were updated
+   * or remained unchanged. If the response is not successful (other than a 304 status), 
+   * an error is thrown indicating the failure to fetch toggles.
+   *
+   * @returns A promise that resolves to a `FetchTogglesStatus` enum, either `Updated`
+   * if new data was fetched or `Unchanged` if no updates were needed.
+   *
+   * @throws Throws an error if the request is not successful (other than a 304 status).
+   */
   public async fetchToggles(): Promise<FetchTogglesStatus> {
     const url = urlWithContextAsQuery(this.url, this.context);
     const response = await fetch(url.toString(), {
@@ -71,6 +97,15 @@ export default class UnleashClient extends EventEmitter {
     return FetchTogglesStatus.Unchanged;
   }
 
+  /**
+   * Retrieves the toggles.
+   * 
+   * Returns the array of toggles if they have been previously downloaded.
+   * If the toggles are not yet downloaded, it throws an error.
+   *
+   * @returns An array of toggles if available.
+   * @throws {Error} If the toggles have not been downloaded.
+   */
   public getToggles(): Toggle[] {
     if (!this.toggles) {
       throw new Error('Toggles not downloaded yet.');
@@ -79,9 +114,23 @@ export default class UnleashClient extends EventEmitter {
     return this.toggles;
   }
 
+  /**
+   * Checks if a toggle with a given name is enabled.
+   * 
+   * This method iterates through the toggles and returns `true` if a toggle with the specified
+   * name is found and is enabled (or false if it's not).
+   *
+   * If the toggles with the given name is not found, it returns `false`.
+   *
+   * An error is thrown if the toggles have not been loaded.
+   *
+   * @param name - The name of the toggle to check.
+   * @returns `true` if the toggle is found and enabled, otherwise `false`.
+   * @throws {Error} If the toggles have not been downloaded.
+   */
   public isEnabled(name: string): boolean {
     if (!this.toggles) {
-      throw new Error('Toggles not loaded.');
+      throw new Error('Toggles not downloaded yet.');
     }
 
     for (let i = 0; i < this.toggles.length; i++) {
