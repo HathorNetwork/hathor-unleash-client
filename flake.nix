11baed67eb02f144e4683eda747366b60c73bf9f
André Abadesso <andre.abadesso@gmail.com>
2024-01-22 11:35:25 -0300

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "virtual environments";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlays.default ];
+        };
+        in
+        pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            nixpkgs-fmt
+            nodejs_20
+            (yarn.override {
+              nodejs = nodejs_20;
+            })
+          ];
+        };
+    });
+}
